63ce711483f5d2c27c6aaafff6bcaf3491235967
govindalakkaraju <govi.csit@gmail.com>
2026-04-13 20:08:17 +0530

    added the logics for the core programms

--- core_skills.py
+++ core_skills.py
@@ -1,14 +1,38 @@
-from collections import defaultdict
+def deduplicate_list_preserving_order(original_list):
+    deduplicated_list = []
+    for item in original_list:
+        if item not in deduplicated_list:
+            deduplicated_list.append(item)
 
-# 1. Flatten this nested dictionary into {"a.b": 1, "a.c.d": 2}
-nested_dict = {"a": {"b": 1, "c": {"d": 2}}}
+    return deduplicated_list
 
-# 2. Deduplicate this list preserving order → [3, 1, 2, 4]
-duplicated_list = [3, 1, 2, 3, 2, 4, 1]
 
-# 3. Group by "dept" → {"eng": ["Alice", "Bob"], "sales": ["Carol"]}
-employees = [
-    {"dept": "eng", "name": "Alice"},
-    {"dept": "eng", "name": "Bob"},
-    {"dept": "sales", "name": "Carol"},
-]
+def group_by_dept_employees(items):
+    result = {}
+    for item in items:
+        if item['dept'] in result:
+            result[item['dept']].append(item['name'])
+        else:
+            result[item['dept']] = [item['name']]
+
+    return result
+
+
+if __name__ == "__main__":
+    # # 1. Flatten this nested dictionary into {"a.b": 1, "a.c.d": 2}
+    # nested_dict = {"a": {"b": 1, "c": {"d": 2}}}
+
+    # 2. Deduplicate this list preserving order → [3, 1, 2, 4]
+    duplicated_list = [3, 1, 2, 3, 2, 4, 1]
+    duplicated_result_list = deduplicate_list_preserving_order(duplicated_list)
+    print(f"Deduplicated list: {duplicated_result_list}")
+
+    # 3. Group by "dept" → {"eng": ["Alice", "Bob"], "sales": ["Carol"]}
+    employees = [
+        {"dept": "eng", "name": "Alice"},
+        {"dept": "eng", "name": "Bob"},
+        {"dept": "sales", "name": "Carol"},
+    ]
+
+    group_by_dept_result = group_by_dept_employees(employees)
+    print(f"Group by dept employees: {group_by_dept_result}")
